8f0b5bb56e673ea23d8ea25e73e37e6a28d44840
ArkadyCH <arkadychu4vaga@gmail.com>
2018-03-17 12:43:37 +0300

    Fix destroy , fix form_word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,9 +2,25 @@ class WordsController < ApplicationController
 	def index
 		@words = Word.all
 	end
+
 	def new
 		@word = Word.new
 	end
+
+	def edit
+		@word = Word.find(params[:id])
+	end
+
+	def update
+		@word = Word.find(params[:id])
+
+		if @word.update_attributes(word_params)
+			redirect_to"/index"
+		else
+			rende :edit
+		end
+	end
+
 	def create
 		@word = Word.new(word_params)
 
@@ -15,6 +31,14 @@ class WordsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@word = Word.find(params[:id])
+		if @word.destroy
+			redirect_to words_path
+		else
+			redirect_to "/404"
+		end
+	end
 	private
 	def word_params
 		params.require(:word).permit(:word , :transfer)
