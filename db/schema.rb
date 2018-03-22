611aa0e5819abad91294fcdec245243c1d3371f5
ArkadyCH <arkadychu4vaga@gmail.com>
2018-03-22 22:04:01 +0300

    fix gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322175237) do
+ActiveRecord::Schema.define(version: 20180322184923) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
